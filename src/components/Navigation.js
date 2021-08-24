fb7740eae76f0b5030903c2a5dfa5de089e81702
Merrrong <h2sc99@hanmail.net>
2021-08-24 21:03:11 +0900

    ver3.0

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import "./Navigation.css"
+
+
+function Navigation(){
+    return <div className="menu">
+        <Link className="home" to="/">Home</Link>
+        <Link className="about" to="/about">About</Link>
+    </div>
+}
+
+export default Navigation;
---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function About(){
+    return <span>
+        About this page : I built it because I love movies.
+    </span>
+}
+
+export default About;
